8358ce286313146d2334f0dfd7c6981ab628b75b
MohamadYouness <mohamad.youness@lau.edu>
2023-03-23 00:59:14 +0200

    final

--- test_script.py
+++ test_script.py
@@ -2,7 +2,10 @@ import requests
 import sys
 import re
 
+
+# the project was done using several functions. According to chatgpt, writing the code as functions is more efficient since it breaks the code into smaller and managebale pieces 
 def make_request(url):
+    # it is a function that enables us to make request to get acces to the URL
     try:
         return requests.get("http://" + url, timeout=5)
     except:
@@ -12,60 +15,69 @@ def make_request(url):
     except:
         pass
 
+
 def get_subdomains(url):
-    with open("subdomains.txt") as sub_file, open("subdomains_output.txt", "w") as out_file:
-        for line in sub_file:
+    # a method that makes a dictionary for the subdomaines 
+    # if the url is valid, it is written in the output.txt file
+    with open("subdomains.txt") as file, open("subdomains_output.txt", "w") as output_file: 
+        for line in file:
             word = line.strip()
             target = word + "." + url
-            sub_response = make_request(target)
-            if sub_response:
-                out_file.write(target + "\n")
+            subdomains_response = make_request(target)
+            if subdomains_response:
+                output_file.write(target + "\n")
+
 
 def get_directories(url):
-    with open("directories.txt") as dir_file, open("directories_output.txt", "w") as out_file:
-        for line in dir_file:
+    # same as get_subdomains but for directories
+    with open("dirs.txt") as file, open("directories_output.txt", "w") as output_file:
+        for line in file:
             word = line.strip()
             target = url + "/" + word
-            dir_response = make_request(target)
-            if dir_response:
-                out_file.write(target + "\n")
+            directories_response = make_request(target)
+            if directories_response:
+                output_file.write(target + "\n")
+
 
 def get_files(url):
-    with open("files_output.txt", "a") as out_file:
+    # a method that makes a dictionary for the files 
+    # if the url is valid, it is written in the output.txt file    
+    with open("files_output.txt", "a") as output_file:
         response = make_request(url)
         if not response:
             return
         html_content = response.content.decode("latin-1")
         files_links = re.findall('(?:href=")(.*?)"', html_content)
-        for file_link in files_links:
-            testing_response = make_request(file_link)
+        for file in files_links:
+            testing_response = make_request(file)
             if testing_response and testing_response.status_code == 200:
-                if check_domain(url, file_link):
-                    get_files(file_link)
+                if check_domain(url, file):
+                    get_files(file)
             else:
                 try:
-                    testing_response = requests.get(file_link)
+                    testing_response = requests.get(file)
                     if not testing_response:
-                        link = url + "/" + file_link
-                        out_file.write(link + "\n")
+                        link = url + "/" + file
+                        output_file.write(link + "\n")
                 except:
-                    link = url + "/" + file_link
-                    out_file.write(link + "\n")
+                    link = url + "/" + file
+                    output_file.write(link + "\n")
+
 
 def check_domain(main_domain, url):
+    # method that checks if the main domain is in the given URL
     return main_domain in url
 
+
 if __name__ == "__main__":
-    target_url = sys.argv[1]
-    response = make_request(target_url)
+    # getting the url from command line arguments and check if it is aa valid one
+    url = sys.argv[1]
+    response = make_request(url)
     if not response:
         print("Invalid URL!")
         sys.exit(0)
 
+    # checking files and subdirectories on the URL after clearing the file
     with open("files_output.txt", "w"):
-        pass  # clear file
-    get_files(target_url)
-    get_subdomains(target_url)
-    get_directories(target_url)
-    
-    print("Scan completed successfully!")
+        pass
+    get_files(url)
